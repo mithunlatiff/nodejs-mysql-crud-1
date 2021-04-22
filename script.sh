a678c00134b4da4976e2f008509ffab505f07851
Latiff, Mithun <Mithun.Latiff@sicpa.com>
2021-04-22 22:47:52 +0800

    Up

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ HOOK_RETRIES=90
 HOOK_SLEEP=10
 curl -s https://raw.githubusercontent.com/mithunlatiff/nodejs-mysql-crud-1/master/database/db.sql -o /tmp/db.sql
 while [ "$HOOK_RETRIES" != 0 ]; do
-    if mysqlshow -h$MYSQL_SERVICE_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE &>/dev/null; then
+    if mysqlshow -htododb -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE &>/dev/null; then
         echo "Database is up"
         break
         else
@@ -16,7 +16,7 @@ if [ "$HOOK_RETRIES" = 0 ]; then
     echo "Unable to connect"
     exit 1
 fi
-if mysql -h$MYSQL_SERVICE_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /tmp/db.sql; then 
+if mysql -htododb -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /tmp/db.sql; then 
     echo "DB Updated success"
 else
     echo "Failed"
